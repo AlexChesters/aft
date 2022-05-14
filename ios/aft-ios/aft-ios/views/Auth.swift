a73f5479448527480a1d27960a1890286e703fbb
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2022-05-14 16:19:31 +0100

    add loading spinner when signed in

--- ios/aft-ios/aft-ios/views/Auth.swift
+++ ios/aft-ios/aft-ios/views/Auth.swift
@@ -12,27 +12,33 @@ struct Auth: View {
     
     @State var email: String = ""
     @State var password: String = ""
+    @State var loading: Bool = false
     
     var body: some View {
         VStack {
-            Spacer()
-            
-            TextField("Email address", text: $email)
-                .keyboardType(.emailAddress)
-                .disableAutocorrection(true)
-                .textInputAutocapitalization(.never)
-            Divider()
-            SecureField("Password", text: $password)
-                .disableAutocorrection(true)
-            Divider()
-            
-            Spacer()
-            
-            Button("Sign in") {
-                self.user.signIn(
-                    email: self.email,
-                    password: self.password
-                )
+            if loading {
+                ProgressView().padding()
+            } else {
+                Spacer()
+                
+                TextField("Email address", text: $email)
+                    .keyboardType(.emailAddress)
+                    .disableAutocorrection(true)
+                    .textInputAutocapitalization(.never)
+                Divider()
+                SecureField("Password", text: $password)
+                    .disableAutocorrection(true)
+                Divider()
+                
+                Spacer()
+                
+                Button("Sign in") {
+                    loading = true
+                    self.user.signIn(
+                        email: self.email,
+                        password: self.password
+                    )
+                }
             }
         }.padding()
     }
