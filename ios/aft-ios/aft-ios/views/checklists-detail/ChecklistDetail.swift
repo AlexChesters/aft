5a7bfeaebdecb38dadd2c4c9fadbeba017b7efa6
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2022-05-15 22:14:19 +0100

    move knowledge about entry completion state to parent

--- ios/aft-ios/aft-ios/views/checklists-detail/ChecklistDetail.swift
+++ ios/aft-ios/aft-ios/views/checklists-detail/ChecklistDetail.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct ChecklistDetail: View {
     let checklist: AFTDataTypes.Checklist
     
+    @State private var completedEntries: [String] = []
+    
     var body: some View {
         ScrollView {
             VStack(alignment: .leading) {
@@ -32,7 +34,24 @@ struct ChecklistDetail: View {
                     ForEach(section.entries, id: \.self) { entry in
                         ChecklistEntry(
                             identifier: checklist.identifier,
-                            entry: entry
+                            entry: entry,
+                            completed: completedEntries.contains(entry),
+                            onTapped: {
+                                let defaults = UserDefaults.standard
+                                
+                                var persistentState = defaults.object(forKey: "\(checklist.identifier)-completed-state") as? [String] ?? []
+                                
+                                if persistentState.contains(entry) {
+                                    persistentState.removeAll(where: { $0 == entry })
+                                } else {
+                                    persistentState.append(entry)
+                                }
+                                
+                                DispatchQueue.main.async {
+                                    defaults.setValue(persistentState, forKey: "\(checklist.identifier)-completed-state")
+                                    completedEntries = persistentState
+                                }
+                            }
                         )
                         Divider()
                     }
@@ -49,6 +68,11 @@ struct ChecklistDetail: View {
             )
         }
         .padding()
+        .onAppear {
+            let defaults = UserDefaults.standard
+            
+            completedEntries = defaults.object(forKey: "\(checklist.identifier)-completed-state") as? [String] ?? []
+        }
     }
 }
 
