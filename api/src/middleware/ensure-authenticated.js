c94c118647789435ec19a93b82f631930022d1e1
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2022-05-15 17:28:05 +0100

    add ios verifier to authentication middlware

--- api/src/middleware/ensure-authenticated.js
+++ api/src/middleware/ensure-authenticated.js
@@ -8,16 +8,27 @@ const webVerifier = CognitoJwtVerifier.create({
   clientId: config.cognito.web.clientId,
   scope: ['email', 'openid']
 })
+const iosVerifier = CognitoJwtVerifier.create({
+  userPoolId: config.cognito.userPoolId,
+  tokenUse: 'access',
+  clientId: config.cognito.ios.clientId,
+  scope: ['email', 'openid']
+})
 
 module.exports = async (req, res, next) => {
   const authHeader = req.get('authorization')
+  const userAgent = req.get('user-agent')
 
-  if (!authHeader) {
+  if (!authHeader || !userAgent) {
     return res.sendStatus(401)
   }
 
+  const verifier = userAgent.startsWith('ios:aft')
+    ? iosVerifier
+    : webVerifier
+
   try {
-    const payload = await webVerifier.verify(authHeader)
+    const payload = await verifier.verify(authHeader)
     req.user = { userId: payload.sub }
     next()
   } catch (err) {
