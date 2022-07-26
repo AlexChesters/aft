c32a5720bd5da45081398f0a79b62c908d88546e
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2022-07-26 23:34:13 +0100

    simplify refresh code

--- api/src/routes/auth/refresh/web.js
+++ api/src/routes/auth/refresh/web.js
@@ -1,47 +1,4 @@
-const fetch = require('node-fetch')
-const { URL } = require('url')
-
+const refresh = require('./')
 const config = require('../../../config')
 
-module.exports = async (req, res, next) => {
-  const { refreshToken } = req.body
-
-  if (!refreshToken) {
-    res.sendStatus(400)
-    return
-  }
-
-  const tokenUrl = new URL('/oauth2/token', config.cognito.domain)
-
-  const params = {
-    grant_type: 'refresh_token',
-    client_id: config.cognito.web.clientId,
-    refresh_token: refreshToken
-  }
-
-  const form = Object
-    .keys(params)
-    .map((key) => {
-      return `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`
-    })
-    .join('&')
-
-  const tokenResponse = await fetch(tokenUrl, {
-    method: 'POST',
-    headers: {
-      'content-type': 'application/x-www-form-urlencoded'
-    },
-    body: form
-  })
-
-  try {
-    const tokens = await tokenResponse.json()
-
-    res.json({
-      accessToken: tokens.access_token,
-      expiresIn: tokens.expires_in
-    })
-  } catch {
-    res.sendStatus(500)
-  }
-}
+module.exports = refresh(config.cognito.web.clientId)
