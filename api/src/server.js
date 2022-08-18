79f4de4cc0292bc789d4226f85f6708277b21f08
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2022-08-18 15:03:56 +0100

    enable secrets

--- api/src/server.js
+++ api/src/server.js
@@ -1,10 +1,10 @@
 require('dotenv').config()
 
-// const setSecrets = require('./utils/set-secrets')
+const setSecrets = require('./utils/set-secrets')
 const config = require('./config')
 
 async function main () {
-  // await setSecrets()
+  await setSecrets()
   require('./app').listen(
     config.port,
     () => console.log(`[${config.environment}] aft-api running on port ${config.port}`)
